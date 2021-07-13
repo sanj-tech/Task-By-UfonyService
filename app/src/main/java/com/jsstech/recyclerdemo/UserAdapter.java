674de1ee94a823ff49567295e32dd80863860c65
Sangeeta J. Pandey <81187698+sanj-tech@users.noreply.github.com>
2021-07-13 14:34:34 +0530

    Update UserAdapter.java

--- app/src/main/java/com/jsstech/recyclerdemo/UserAdapter.java
+++ app/src/main/java/com/jsstech/recyclerdemo/UserAdapter.java
@@ -2,12 +2,16 @@ package com.jsstech.recyclerdemo;
 
 import android.app.LauncherActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Response;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,11 +19,12 @@ import java.util.List;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class UserAdapter extends RecyclerView.Adapter<UserAdapter.MyViewHolder>{
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.MyViewHolder> {
+
     private List<UserModel> listItems;
     private Context context;
 
-    public UserAdapter(List<UserModel> listItems,Context context) {
+    public UserAdapter(Context context,List<UserModel> listItems) {
         this.listItems = listItems;
         this.context = context;
     }
@@ -28,17 +33,38 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.MyViewHolder>{
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent,int viewType) {
-        View view= LayoutInflater.from(context).inflate(R.layout.item_layout,parent,false);
-                //inflate(R.layout.item_layout,parent,false);
+        View view = LayoutInflater.from(context).inflate(R.layout.item_layout,parent,false);
         return new MyViewHolder(view);
+
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder,int position) {
-        UserModel model= (UserModel) listItems.get(position);
-   holder.txtdate.setText(((UserModel) listItems.get(position)).getExp());
-        holder.txtdate.setText(((UserModel) listItems.get(position)).getDates());
 
+        holder.txtdes.setText(listItems.get(position).getDescription());
+
+        Picasso.get().load(listItems.get(position).getUrl()).into(holder.imageView);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context,"show msg",Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+//        holder.imageView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                holder.itemView.
+////                Intent intent = new Intent(context,ShowImageData.class);
+//////                intent.putExtra("description",listItems.get(position).getUrl());
+//////
+////                context.startActivity(intent);
+//
+//            }
+//        });
 
     }
 
@@ -48,15 +74,16 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.MyViewHolder>{
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
-        TextView txtname;
-        TextView txtdate;
+        ImageView imageView;
+
+        TextView txtdes;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            txtname=itemView.findViewById(R.id.tname);
-            txtdate=itemView.findViewById(R.id.tdate);
+            imageView = itemView.findViewById(R.id.img);
 
+            txtdes = itemView.findViewById(R.id.explain);
 
         }
     }
